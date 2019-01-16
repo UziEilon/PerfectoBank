a022e31c8a1f4767bc273dcc9071369faab72946
Uzi Eilon <uzie@perfectomobile.com>
2019-01-16 10:43:27 -0500

    Order the tests

--- src/main/java/com/quantum/tests/PerfectoBank.java
+++ src/main/java/com/quantum/tests/PerfectoBank.java
@@ -61,7 +61,7 @@ public class PerfectoBank extends WebDriverTestCase {
 
     }
 
-    @Test(dependsOnMethods={"transferMoneyMoreThenBalance"} ,description="transfer", groups={"perfectoBank","P1","search"})
+    @Test(dependsOnMethods={"PerfectoBankCheckDeposit"} ,description="transfer", groups={"perfectoBank","P1","search"})
     public void transferMoneyMoreThenBalance() {
 
         getDriver().findElement(By.xpath("//*[@label=\"Transfer by email\"]")).click();
